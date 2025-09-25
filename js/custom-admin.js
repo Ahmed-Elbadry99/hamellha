f1b451058291a3e4ffe6c390afed9e6ca4019169
Ahmed Elbadry <elbadry.egypt010@gmail.com>
2025-09-25 16:06:01 +0300

    add details

--- js/custom-admin.js
+++ js/custom-admin.js
@@ -72,3 +72,93 @@ document.addEventListener('click', function(e) {
     }
   });
 });
+
+
+
+
+
+// toggle filter
+const toggleBtn = document.getElementById("toggleFilter");
+
+ if(toggleBtn){
+     const filtersBox = document.getElementById("filtersBox");
+
+    // فتح/غلق عند الضغط على الزر
+    toggleBtn.addEventListener("click", function (e) {
+        e.stopPropagation(); // عشان ما يقفلش مباشرة
+        filtersBox.classList.toggle("show");
+    });
+
+    // قفل عند الضغط خارج الفلتر
+    document.addEventListener("click", function (e) {
+        if (!filtersBox.contains(e.target) && e.target !== toggleBtn) {
+            filtersBox.classList.remove("show");
+        }
+    });
+
+ }
+
+
+
+
+ 
+    // details slider
+
+
+
+    function initSliders() {
+  // Unslick لو كانت متفعلة بالفعل
+  if ($('.slider-for').hasClass('slick-initialized')) {
+    $('.slider-for').slick('unslick');
+  }
+  if ($('.slider-nav').hasClass('slick-initialized')) {
+    $('.slider-nav').slick('unslick');
+  }
+
+  // إعادة تهيئة slider-for
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+
+  // إعداد slider-nav حسب العرض
+  const isMobile = $(window).width() <= 992;
+
+  $('.slider-nav').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true,
+    vertical: !isMobile // عمودي فقط إذا الشاشة أكبر من 992
+  });
+}
+
+// أول تحميل
+$(document).ready(function () {
+  initSliders();
+});
+
+// إعادة تهيئة عند تغيير حجم الشاشة
+$(window).on('resize', function () {
+  clearTimeout(window.resizingSlick); // تأخير منعًا للتكرار
+  window.resizingSlick = setTimeout(function () {
+    initSliders();
+  }, 300);
+});
+
+
+
+
+// 
+
+ document.querySelectorAll(".delete-btn").forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const accordionItem = this.closest(".accordion-item");
+      accordionItem.remove();
+    });
+  });
